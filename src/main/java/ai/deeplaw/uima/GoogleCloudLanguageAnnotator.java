c5a61e9874f24bf9957d5c8c04ec84457efca693
Kasper Marstal <kaspermarstal@gmail.com>
2016-09-08 14:51:16 +0200

    STYLE: Indentation

--- src/main/java/ai/deeplaw/uima/GoogleCloudLanguageAnnotator.java
+++ src/main/java/ai/deeplaw/uima/GoogleCloudLanguageAnnotator.java
@@ -8,64 +8,70 @@ import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.services.language.v1beta1.CloudNaturalLanguageAPI;
 import com.google.api.services.language.v1beta1.CloudNaturalLanguageAPIScopes;
-import com.google.api.services.language.v1beta1.model.AnalyzeEntitiesRequest;
-import com.google.api.services.language.v1beta1.model.AnalyzeEntitiesResponse;
+import com.google.api.services.language.v1beta1.model.AnnotateTextRequest;
+import com.google.api.services.language.v1beta1.model.AnnotateTextResponse;
 import com.google.api.services.language.v1beta1.model.Document;
 import com.google.api.services.language.v1beta1.model.Entity;
+import com.google.api.services.language.v1beta1.model.Features;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas;
+import org.apache.uima.jcas.tcas.Annotation;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
 
 public class GoogleCloudLanguageAnnotator extends JCasAnnotator_ImplBase {
 
-  private final CloudNaturalLanguageAPI api;
+  private final CloudNaturalLanguageAPI languageApi;
 
-  public GoogleCloudLanguageAnnotator(APPLICATION_NAME)  throws IOException, GeneralSecurityException {
-    final GoogleCredential credential = GoogleCredential
-      .getApplicationDefault()
-      .createScoped(CloudNaturalLanguageAPIScopes.all());
+  public GoogleCloudLanguageAnnotator()  throws IOException, GeneralSecurityException {
+    // TODO: Set APPLICATION_NAME from configuration
+    final String APPLICATION_NAME = "";
 
-    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    final GoogleCredential credential =
+      GoogleCredential
+        .getApplicationDefault()
+        .createScoped(CloudNaturalLanguageAPIScopes.all());
 
-    api = new CloudNaturalLanguageAPI.Builder(
-      GoogleNetHttpTransport.newTrustedTransport(),
-      jsonFactory,
-      new HttpRequestInitializer() {
-        @Override
-        public void initialize(HttpRequest request) throws IOException {
-          credential.initialize(request);
-        }
-      })
-      .setApplicationName(APPLICATION_NAME)
-      .build();
-
+    languageApi =
+      new CloudNaturalLanguageAPI
+        .Builder(
+          GoogleNetHttpTransport.newTrustedTransport(),
+          JacksonFactory.getDefaultInstance(),
+          new HttpRequestInitializer() {
+          @Override
+          public void initialize(HttpRequest request) throws IOException {
+            credential.initialize(request);
+          }
+        })
+        .setApplicationName(APPLICATION_NAME)
+        .build();
   }
 
-  public List<Entity> analyzeEntities(String text) throws IOException {
-    AnalyzeEntitiesRequest request =
-      new AnalyzeEntitiesRequest()
+  public AnnotateTextResponse annotateText(String text) throws IOException {
+    AnnotateTextRequest request =
+      new AnnotateTextRequest()
         .setDocument(new Document()
           .setContent(text)
           .setType("PLAIN_TEXT"))
-        .setEncodingType("UTF16");
+        .setEncodingType("UTF16")
+        .setFeatures(new Features().setExtractSyntax(false))
+        .setFeatures(new Features().setExtractEntities(true))
+        .setFeatures(new Features().setExtractDocumentSentiment(false));
 
-    CloudNaturalLanguageAPI.Documents.AnalyzeEntities entitiesAnalyzer =
-      api.documents().analyzeEntities(request);
-    AnalyzeEntitiesResponse response = entitiesAnalyzer.execute();
+    CloudNaturalLanguageAPI.Documents.AnnotateText annotateTextRequest =
+      languageApi
+        .documents()
+        .annotateText(request);
 
-    return response.getEntities();
+    return annotateTextRequest.execute();
   }
 
-  public void process(JCas aJCas){
+  public void process(JCas aJCas) {
     String text = aJCas.getDocumentText();
     try {
-      List<Entity> entities = analyzeEntities(text);
-      for( Entity entity : entities) {
+      AnnotateTextResponse annotateTextResponse = annotateText(text);
+      for( Entity entity : annotateTextResponse.getEntities() ) {
         // add to aJcas
       }
     } catch(IOException e) {
